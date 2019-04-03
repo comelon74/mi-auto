47f68d6764f7481f6b813a5c2f1b99a1029ff4a1
comelon74 <interned_123@hotmail.com>
2019-04-03 13:39:22 -0600

    Agregue ngFor para que los divs de productos se agreguen desde un array

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -6,7 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
-
+ items=[
+   {
+     ima:"assets/ima/slider/carro1.jpg",
+     modelo:"Auto modelo 1",
+     descripcion:"Este auto es genial",
+     precio:15000
+   },
+   {
+    ima:"assets/ima/slider/carro2.jpg",
+    modelo:"Auto modelo 2",
+    descripcion:"Este auto tambien es genial",
+    precio:18000
+  },
+  {
+    ima:"assets/ima/slider/carro3.jpg",
+    modelo:"Auto modelo 3",
+    descripcion:"Este auto no es tan genial",
+    precio:21000
+  }
+ ]
   constructor() { }
 
   ngOnInit() {
